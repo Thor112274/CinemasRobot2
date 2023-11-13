2bc7398c9bf7a73f19e5736173a6dc0cd0485a89
Walter3699 <149938033+Walter3699@users.noreply.github.com>
2023-11-13 22:30:21 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -7,7 +7,7 @@ class script(object):
 Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Mʏ Cᴏᴍᴍᴀɴᴅs.</b>"""
  
     ABOUT_TXT = """<b>⍟───[ ᴍʏ ᴅᴇᴛᴀɪʟꜱ ]───⍟
- ‣ ᴍʏ ɴᴀᴍᴇ : ᴍɪɴᴀᴛᴏ ɴᴀᴍɪᴋᴀᴢᴇ
+ ‣ ᴍʏ ɴᴀᴍᴇ : ᴀᴋ ᴇɴᴛᴇʀᴛᴀɪɴᴍᴇɴᴛs ғɪʟᴇs ᴮᵒᵗ 🤖
  ‣ ᴍʏ ʙᴇꜱᴛ ꜰʀɪᴇɴᴅ: <a href='tg://settings'>ᴛʜɪs ᴘᴇʀsᴏɴ</a> 
  ‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/Ak_Entertainments_ofcl_bot'>ᴀᴋ ᴇɴᴛᴇʀᴛᴀɪɴᴍᴇɴᴛs</a> 
  ‣ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org//'>ᴘʏʀᴏɢʀᴀᴍ</a> 
