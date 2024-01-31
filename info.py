31eac6792d5266e888de2f16cf90c81125ae67df
cultcinemas <152758474+cultcinemas@users.noreply.github.com>
2024-01-31 22:35:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://cultcinemas:cultcinemas@cluster0.xvzi9hw.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'ClutCinemas_DB')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'CultCinemasDB')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
